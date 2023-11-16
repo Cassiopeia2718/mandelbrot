e481ffa1c6645c0bb628050dd9a5b33e58cad756
Nicholas <139596475+Cassiopeia2718@users.noreply.github.com>
2023-11-16 07:54:29 -0800

    styling Mandelbrot.py

--- Mandelbrot.py
+++ Mandelbrot.py
@@ -2,34 +2,34 @@ import numpy as np
 from matplotlib import pyplot as plt
 import math
 
-def mandelbrot(min_X, max_X, min_Y, max_Y, width_pixels, itterations, file_name):
+def mandelbrot(minX, maxX, minY, maxY, widthPixels, iterations, fileName):
 
-    pixel_density = (max_X - min_X)/width_pixels
+    pixelDensity = (maxX - minX)/widthPixels
 
-    a = np.arange(min_X, max_X + pixel_density, pixel_density)
-    b = np.arange(min_Y, max_Y + pixel_density, pixel_density)
+    a = np.arange(minX, maxX + pixelDensity, pixelDensity)
+    b = np.arange(minY, maxY + pixelDensity, pixelDensity)
 
     A = np.tile(a, (len(b), 1))
     B = np.ndarray.transpose(np.tile(b, (len(a), 1)))
 
-    Plane = np.empty(np.shape(A), dtype=np.complex128)
+    plane = np.empty(np.shape(A), dtype=np.complex128)
 
     Plane.real = A
     Plane.imag = B
 
-    Mandelbrot = np.sqrt(isStable(Plane, itterations))
+    mandelbrot = np.sqrt(isStable(plane, iterations))
 
-    plt.imshow(Mandelbrot)
-    plt.imsave("/Users/nicholas/Desktop/Fractels/" + file_name + ".png", Mandelbrot)
+    plt.imshow(mandelbrot)
+    plt.imsave("/Users/nicholas/Desktop/Fractels/" + file_name + ".png", mandelbrot)
     plt.show()
      
-def isStable(z, itterations):
+def isStable(z, iterations):
 
     z_new = np.zeros(np.shape(z))
     z_color = np.zeros(np.shape(z)) 
     i = 0
 
-    while i < itterations:
+    while i < iterations:
 
         z_new = np.square(z_new) + z
         z_color[np.where(np.square(np.real(z_new)) + np.square(np.imag(z_new)) >= 2)] = i
@@ -37,13 +37,13 @@ def isStable(z, itterations):
         print(i)
         i+=1
 
-        if i == itterations:
+        if i == iterations:
 
             plt.imshow(z_color)
             plt.show()
 
             if input("Are you satisfid(yes/no): ") == "no":
-                itterations += int(input("Number of itterations more(int): "))
+                iterations += int(input("Number of iterations more(int): "))
 
     return z_color
 
